58911fc1c9cb4fd4f362a1b817a7ed31693e6147
Gilda <your-email@example.com>
2026-09-17 10:40:56 +1000

    Day 1: Python basics and personal profile generator

---
+++ Python-Practice/day1.py
@@ -0,0 +1,9 @@
+name = "admin"
+experience = 10
+career ="front end web developer"
+technology = "Python"
+
+print ("My name is", name)
+print ("My experience is ", experience)
+print ("my career is ", career)
+print("I am learning" , technology)
---
+++ Python-Practice/input_practice.py
@@ -0,0 +1,18 @@
+name = input("what is your name?")
+career = input ("what is your career ?")
+experience = int(input("how many years experience do you have?"))
+technology = input("what do you want to learn?")
+goal =input("what is your furture goal?")
+next_year_experience = experience +1
+
+print(f"name :{name}")
+print (f"Career: {career}")
+print (f"Experience : {experience} years ")
+print(f"Current learning: {technology}")
+print(f"my future goal :{goal}")
+print (f"next year you will have {next_year_experience} years of experience")
+if experience >=10: print("you are expert") 
+elif experience >=5:
+    print ("you can do it but you should work hard  ")
+else:
+    print (" you have to practice alot, you are continuing to build your professional experience ")
